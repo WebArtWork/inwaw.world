d1251364b03c8c2bfaa8e0716692a0305437e53e
Nika <danzi6386@gmail.com>
2025-06-19 12:45:02 +0300

    testing safe area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { SafeArea } from '@capacitor-community/safe-area';
+
+SafeArea.enable({
+	config: {
+		customColorsForSystemBars: true,
+		statusBarColor: '#00000000', // transparent
+		statusBarContent: 'light',
+		navigationBarColor: '#00000000', // transparent
+		navigationBarContent: 'light'
+	}
+});
 
 @Component({
 	selector: 'app-root',
